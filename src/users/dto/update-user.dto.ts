955f5a72573c3ec68db2b8e2afd2290a6e0d7ab4
MaxSavostyanov <maxsavostyanov34@gmail.com>
2024-06-15 00:09:11 +0300

    fix: error update user

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,13 +1,16 @@
 import { IsEmail, IsOptional, IsString, IsUrl, Length } from 'class-validator';
 
 export class UpdateUserDto {
+  @IsOptional()
   @IsString()
   @Length(1, 64)
   username: string;
 
+  @IsOptional()
   @IsEmail()
   email: string;
 
+  @IsOptional()
   @IsString()
   @Length(2)
   password: string;
